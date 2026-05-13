4fe4316ea50f39f91f70ca6a201bd9b62bf260f1
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-13 21:28:16 +0200

    Fix syntax

--- include/Building.hpp
+++ include/Building.hpp
@@ -1,9 +1,24 @@
+#pragma once
 
 class Building
 {
-	void setOnFire();
-	void extinguishFire();
-	void decayToRuin();
-	void restoreRuin();
-	virtual applyProbabilities(/*<lambdy-funkcji-symulacji>*/);
-}
+public:
+    virtual ~Building() = default;
+
+    Building(const Building&) = delete;
+    auto operator=(const Building&) -> Building& = delete;
+
+    Building(Building&&) = default;
+    auto operator=(Building&&) -> Building& = default;
+
+    void setOnFire();
+    void extinguishFire();
+    void decayToRuin();
+    void restoreRuin();
+    
+    virtual void applyProbabilities();
+
+protected:
+    // Constructor (protected if you don't want direct instantiation of 'Building')
+    Building() = default;
+};
