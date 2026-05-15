525f1f49275636f359b1cd5fba79b0fbff09afff
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-15 13:36:51 +0200

    Add cell exist and cell set contents functionality to Board

--- include/Board.hpp
+++ include/Board.hpp
@@ -15,15 +15,26 @@ class Board
       , height(height)
       , contents(width * height) {};
     [[nodiscard]]
+    auto checkCellExistsAtCoordinates(Coordinates const& coordinates) const -> bool;
+    [[nodiscard]]
     auto calculateIndexFromCoordinates(Coordinates const& coordinates) const -> unsigned int;
     [[nodiscard]]
     auto getWidth() const -> unsigned int;
     [[nodiscard]]
     auto getHeight() const -> unsigned int;
+    void setContentsValueAtCoordinates(Coordinates const& coordinates, T value);
     /*BuildingType getCellBuildingType(Coordinates const& coordinates);*/
     /*auto getCellOccupant(Coordinates coords) -> CellOccupant *;*/
     /*void applyProbability(Coordinates const& coordinates); */
 };
+
+template<typename T>
+auto Board<T>::checkCellExistsAtCoordinates(Coordinates const& coordinates) const -> bool
+{
+    if (coordinates.x < 0 || coordinates.x >= width) return false;
+    if (coordinates.y < 0 || coordinates.y >= height) return false;
+    return true;
+}
 
 template<typename T>
 auto Board<T>::calculateIndexFromCoordinates(Coordinates const& coordinates) const -> unsigned int
@@ -41,4 +52,11 @@ template<typename T>
 auto Board<T>::getHeight() const -> unsigned int
 {
     return height;
+}
+
+template<typename T>
+void Board<T>::setContentsValueAtCoordinates(Coordinates const& coordinates, T value)
+{
+    unsigned int index = calculateIndexFromCoordinates(coordinates);
+    contents.at(index) = value;
 }
