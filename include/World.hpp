de50c9dbd66f15d0f3c662021bd2bdb7c15af13a
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-18 00:52:22 +0200

    Add usefull constants for global usage

--- include/World.hpp
+++ include/World.hpp
@@ -1,10 +1,25 @@
 #pragma once
+#include <cstdint>
 
-enum class BuildingType
+enum class BuildingType : std::uint8_t
 {
     HOUSE,
     SHOP,
     FIRESTATION,
     CHURCH,
     FACTORY,
+    NONE,
 };
+
+enum class ProbabilityType : std::uint8_t
+{
+    CREATE_NEW_BUILDING,
+    SET_CURRENT_BUILDING_ON_FIRE,
+};
+
+struct ProbabilityDefaultPercentages
+{
+    double create_new_building{ 0.01 };
+};
+
+constexpr unsigned int default_properties_house_radius = 1;
