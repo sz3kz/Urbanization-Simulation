4cf53167bacd15c7650580f6124a918a8c8653e9
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-18 00:41:44 +0200

    Implement House building object class

--- include/House.hpp
+++ include/House.hpp
@@ -1,6 +1,24 @@
 #pragma once
 #include "Building.hpp"
+#include "Coordinates.hpp"
+#include <functional>
 
-class House : public Building {
-    House();
+class House : public Building
+{
+    unsigned int radius;
+
+  public:
+    explicit House(unsigned int supplied_radius)
+      : radius(supplied_radius) {};
+    [[nodiscard]]
+    auto getBuildingType() const -> BuildingType override;
+
+    void applyProbabilities(
+      std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+      std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+      std::function<bool(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageIsSetAtCoordinates,
+      std::function<double(Coordinates, ProbabilityType)> askProbabilityTypePercentageAtCoordinates,
+      std::function<void(Coordinates, ProbabilityType, double)>
+        setCellPercentageOfProbabilityAtCoordinates) override;
 };
