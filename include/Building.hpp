53ba56da6ddd21ebbc11031a77bb1755fa01064f
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-18 00:24:38 +0200

    Implement Building interface for building object classes

--- include/Building.hpp
+++ include/Building.hpp
@@ -1,26 +1,40 @@
 #pragma once
+#include "Coordinates.hpp"
+#include "World.hpp"
+#include <functional>
+
+// virtual <return-type> func(<parameters>) = 0
+//  Pure virtual function: don't implement it here, force implementation at child,
+//  disallow direct Building object instatiation.
 
 class Building
 {
-public:
+  public:
+    /* Virtual destructor
+     * Can aid in childclass identification via dynamic_cast.
+     * Since we use the Building::getBuildingType() function to identify childclasses
+     *  this is not really needed
+     */
     virtual ~Building() = default;
 
-    Building(const Building&) = delete;
-    auto operator=(const Building&) -> Building& = delete;
+    /* Disable all other special methods*/
+    Building(Building const&) = delete;
+    auto operator=(Building const&) -> Building& = delete;
+    Building(Building&&) = delete;
+    auto operator=(Building&&) -> Building& = delete;
 
-    Building(Building&&) = default;
-    auto operator=(Building&&) -> Building& = default;
+    [[nodiscard]]
+    virtual auto getBuildingType() const -> BuildingType = 0;
+    // Function that populates probability_board with probabilities
+    virtual void applyProbabilities(
+      std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+      std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+      std::function<bool(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageIsSetAtCoordinates,
+      std::function<double(Coordinates, ProbabilityType)> getProbabilityTypePercentageAtCoordinates,
+      std::function<void(Coordinates, ProbabilityType, double)>
+        setProbabilityTypePercentageAtCoordinates) = 0;
 
-    void setOnFire();
-    void extinguishFire();
-    void decayToRuin();
-    void restoreRuin();
-    
-    virtual void applyProbabilities();
-
-    void doTurn();
-
-protected:
-    // Constructor (protected if you don't want direct instantiation of 'Building')
+  protected:
     Building() = default;
 };
