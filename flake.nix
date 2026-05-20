eec90a1799e002c68b269bd0ef8f7e8632dabcbc
Aradziem <adammiek9@outlook.com>
2026-05-20 11:56:42 +0200

    clion

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
               llvm.clang-tools    # Needed to expose system headers to clang-tidy
             ];
             buildInputs = [
+              pkgs.jetbrains.clion
               pkgs.doxygen
               pkgs.gcc
               pkgs.cmake
