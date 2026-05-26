ae9907b4f98a9ba86865d41bec062b4657ad6a94
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-26 20:40:11 +0200

    Implement Factory building class

--- include/Factory.hpp
+++ include/Factory.hpp
@@ -1,6 +1,25 @@
 #pragma once
 #include "Building.hpp"
+#include "Coordinates.hpp"
+#include <functional>
 
-class Factory : public Building {
-    Factory();
+class Factory : public Building
+{
+  public:
+    explicit Factory(unsigned int supplied_radius)
+      : Building(supplied_radius) {};
+    [[nodiscard]]
+    auto getBuildingType() const -> BuildingType override;
+
+    void applyProbabilities(
+      [[maybe_unused]] std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageIsSetAtCoordinates,
+      [[maybe_unused]] std::function<double(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageAtCoordinates,
+      [[maybe_unused]] std::function<void(Coordinates, ProbabilityType, double)>
+        setCellPercentageOfProbabilityAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates, std::string)>
+        askBuildingAtCoordinatesIsInState) override;
 };
---
+++ src/buildings/Factory.cpp
@@ -0,0 +1,88 @@
+#include "Factory.hpp"
+#include "Coordinates.hpp"
+#include "World.hpp"
+#include <iostream>
+
+constexpr double restore_from_ruin_probability_initial_value = 0.3;
+
+auto Factory::getBuildingType() const -> BuildingType
+{
+    return BuildingType::FACTORY;
+}
+
+void Factory::applyProbabilities(
+  [[maybe_unused]] std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates, ProbabilityType)>
+    askProbabilityTypePercentageIsSetAtCoordinates,
+  [[maybe_unused]] std::function<double(Coordinates, ProbabilityType)>
+    askProbabilityTypePercentageAtCoordinates,
+  [[maybe_unused]] std::function<void(Coordinates, ProbabilityType, double)>
+    setCellPercentageOfProbabilityAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates, std::string)> askBuildingAtCoordinatesIsInState)
+{
+    for (int i = (-1) * static_cast<int>(radius); i <= static_cast<int>(radius); ++i)
+    {
+        for (int j = (-1) * static_cast<int>(radius); j <= static_cast<int>(radius); ++j)
+        {
+            bool is_self = (i == 0 && j == 0);
+            if (is_self)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_HOUSE, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_FIRESTATION, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_SHOP, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_FACTORY, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_CHURCH, 0.0);
+                continue;
+            }
+
+            bool exists = askCellExistsAtCoordinates(Coordinates(i, j));
+            if (!exists)
+            {
+                continue;
+            }
+
+            bool empty = askCellEmptyAtCoordinates(Coordinates(i, j));
+            if (empty)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_FACTORY, 0.0);
+                continue;
+            }
+
+            bool self_is_on_fire = (getStateName() == "Burning");
+            bool in_closes_neighbourhood = (i * i + j * j <= 2);
+            bool is_ruin = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Ruin");
+            bool is_on_fire = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Burning");
+            bool is_cell_probability_already_set = askProbabilityTypePercentageIsSetAtCoordinates(
+              Coordinates(i, j), ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE);
+            if (self_is_on_fire && in_closes_neighbourhood && !is_ruin && !is_on_fire &&
+                !is_cell_probability_already_set)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j),
+                  ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE,
+                  set_adjacent_building_on_fire);
+            }
+
+            bool self_is_normal = askBuildingAtCoordinatesIsInState(Coordinates(0, 0), "Normal");
+            if (self_is_normal && is_ruin)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j),
+                  ProbabilityType::RESTORE_FROM_RUIN,
+                  restore_from_ruin_probability_initial_value);
+            }
+
+            /*
+            setCellPercentageOfProbabilityAtCoordinates(
+              Coordinates(i, j), ProbabilityType::CREATE_NEW_FIRESTATION, 0.0);
+            */
+        }
+    }
+}
