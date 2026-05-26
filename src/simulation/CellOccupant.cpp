aef583855e87fc77d4b09447c3ff464fc87c97da
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-26 20:49:16 +0200

    Account operator overloading for specific building classes

--- src/simulation/CellOccupant.cpp
+++ src/simulation/CellOccupant.cpp
@@ -7,13 +7,43 @@ auto CellOccupant::release() -> std::unique_ptr<Building>
 
 auto operator<<(std::ostream& os, CellOccupant const& cell_occupant) -> std::ostream&
 {
-    if (cell_occupant.occupant)
-    {
-        os << "🏠";
-    }
-    else
+    if (cell_occupant.occupant == nullptr)
     {
         os << "🟦";
+        return os;
+    }
+    Building const* building = cell_occupant.getBuilding();
+
+    if (building->getStateName() == "Burning")
+    {
+        os << "🔥";
+    }
+    else if (building->getStateName() == "Ruin")
+    {
+        os << "🏚️";
+        // os << "🟫";
+    }
+    else if (building->getBuildingType() == BuildingType::HOUSE)
+    {
+        os << "🏠";
+        // os << "🟩";
+    }
+    else if (building->getBuildingType() == BuildingType::FIRESTATION)
+    {
+        os << "🚒";
+    }
+    else if (building->getBuildingType() == BuildingType::SHOP)
+    {
+        os << "🏪";
+    }
+    else if (building->getBuildingType() == BuildingType::FACTORY)
+    {
+        os << "🏭";
+    }
+    else if (building->getBuildingType() == BuildingType::CHURCH)
+    {
+        os << "⛪";
+        // os << "🫙";
     }
     return os;
 }
