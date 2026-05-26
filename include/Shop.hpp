0ce3d17efe16c6df984a8779f6d0bb05c87f524b
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-05-26 20:38:27 +0200

    Implement Shop building class

--- include/Shop.hpp
+++ include/Shop.hpp
@@ -1,6 +1,25 @@
 #pragma once
 #include "Building.hpp"
+#include "Coordinates.hpp"
+#include <functional>
 
-class Shop : public Building {
-    Shop();
+class Shop : public Building
+{
+  public:
+    explicit Shop(unsigned int supplied_radius)
+      : Building(supplied_radius) {};
+    [[nodiscard]]
+    auto getBuildingType() const -> BuildingType override;
+
+    void applyProbabilities(
+      [[maybe_unused]] std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageIsSetAtCoordinates,
+      [[maybe_unused]] std::function<double(Coordinates, ProbabilityType)>
+        askProbabilityTypePercentageAtCoordinates,
+      [[maybe_unused]] std::function<void(Coordinates, ProbabilityType, double)>
+        setCellPercentageOfProbabilityAtCoordinates,
+      [[maybe_unused]] std::function<bool(Coordinates, std::string)>
+        askBuildingAtCoordinatesIsInState) override;
 };
---
+++ src/buildings/Shop.cpp
@@ -0,0 +1,96 @@
+#include "Shop.hpp"
+#include "Coordinates.hpp"
+#include "World.hpp"
+#include <iostream>
+
+constexpr double shop_restore_time_to_live_boost = 0.001;
+
+auto Shop::getBuildingType() const -> BuildingType
+{
+    return BuildingType::SHOP;
+}
+
+void Shop::applyProbabilities(
+  [[maybe_unused]] std::function<bool(Coordinates)> askCellExistsAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates)> askCellEmptyAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates, ProbabilityType)>
+    askProbabilityTypePercentageIsSetAtCoordinates,
+  [[maybe_unused]] std::function<double(Coordinates, ProbabilityType)>
+    askProbabilityTypePercentageAtCoordinates,
+  [[maybe_unused]] std::function<void(Coordinates, ProbabilityType, double)>
+    setCellPercentageOfProbabilityAtCoordinates,
+  [[maybe_unused]] std::function<bool(Coordinates, std::string)> askBuildingAtCoordinatesIsInState)
+{
+    for (int i = (-1) * static_cast<int>(radius); i <= static_cast<int>(radius); ++i)
+    {
+        for (int j = (-1) * static_cast<int>(radius); j <= static_cast<int>(radius); ++j)
+        {
+            bool is_self = (i == 0 && j == 0);
+            if (is_self)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_HOUSE, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_FIRESTATION, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_SHOP, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_FACTORY, 0.0);
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_CHURCH, 0.0);
+                bool is_normal = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Normal");
+                if (is_normal)
+                {
+                    setCellPercentageOfProbabilityAtCoordinates(
+                      Coordinates(i, j),
+                      ProbabilityType::RESTORE_TIME_TO_LIVE,
+                      shop_restore_time_to_live_boost);
+                }
+                else
+                {
+                    setCellPercentageOfProbabilityAtCoordinates(
+                      Coordinates(i, j), ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE, 0.0);
+                }
+                continue;
+            }
+
+            bool exists = askCellExistsAtCoordinates(Coordinates(i, j));
+            if (!exists)
+            {
+                continue;
+            }
+
+            bool empty = askCellEmptyAtCoordinates(Coordinates(i, j));
+            if (empty)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j), ProbabilityType::CREATE_NEW_SHOP, 0.0);
+                continue;
+            }
+
+            bool self_is_on_fire = (getStateName() == "Burning");
+            bool in_closes_neighbourhood = (i * i + j * j <= 2);
+            bool is_ruin = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Ruin");
+            bool is_on_fire = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Burning");
+            bool is_cell_probability_already_set = askProbabilityTypePercentageIsSetAtCoordinates(
+              Coordinates(i, j), ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE);
+            if (self_is_on_fire && in_closes_neighbourhood && !is_ruin && !is_on_fire &&
+                !is_cell_probability_already_set)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(
+                  Coordinates(i, j),
+                  ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE,
+                  set_adjacent_building_on_fire);
+            }
+
+            bool self_is_normal = askBuildingAtCoordinatesIsInState(Coordinates(0, 0), "Normal");
+            bool is_normal = askBuildingAtCoordinatesIsInState(Coordinates(i, j), "Normal");
+            if (is_normal && self_is_normal)
+            {
+                setCellPercentageOfProbabilityAtCoordinates(Coordinates(i, j),
+                                                            ProbabilityType::RESTORE_TIME_TO_LIVE,
+                                                            shop_restore_time_to_live_boost);
+            }
+        }
+    }
+}
