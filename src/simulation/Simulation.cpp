ef9a3f873828479bc1825651f3843a4ce28db10e
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-06-02 19:31:03 +0200

    refractor: const-ify probability elements

--- src/simulation/Simulation.cpp
+++ src/simulation/Simulation.cpp
@@ -115,7 +115,7 @@ void Simulation::executeProbability()
                 /* OMG THIS CASE SWITCH DICTATES PROBABILITY PRECEDENSE */
                 /* I HAVE TO KEEP TRACK IF AN EMPTY CELL WAS POPULATED WITH A BOOL*/
                 /* JESUS CHRIST PLEASE HELP ME */
-                auto& [last_modified_iteration, value] = probability;
+                const auto& [last_modified_iteration, value] = probability;
                 switch (probability_type)
                 {
                     case (ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE):
