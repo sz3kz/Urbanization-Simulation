5347ada86ca0c4fa388189fa6b933035ff13e4b6
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-06-02 21:44:17 +0200

    cleanup: delete no-longer-valid commented-out code

--- src/simulation/CellOccupant.cpp
+++ src/simulation/CellOccupant.cpp
@@ -23,34 +23,10 @@ auto operator<<(std::ostream& os, CellOccupant const& cell_occupant) -> std::ost
     else if (building->getBuildingState() == BuildingState::RUIN)
     {
         os << "🏚️";
-        // os << "🟫";
     }
     else
     {
         os << building_images[building->getBuildingType()];
     }
-    /* remove this if not needed anymore
-    else if (building->getBuildingType() == BuildingType::HOUSE)
-    {
-        os << "🏠";
-        // os << "🟩";
-    }
-    else if (building->getBuildingType() == BuildingType::FIRESTATION)
-    {
-        os << "🚒";
-    }
-    else if (building->getBuildingType() == BuildingType::SHOP)
-    {
-        os << "🏪";
-    }
-    else if (building->getBuildingType() == BuildingType::FACTORY)
-    {
-        os << "🏭";
-    }
-    else if (building->getBuildingType() == BuildingType::CHURCH)
-    {
-        os << "⛪";
-        // os << "🫙";
-    }*/
     return os;
 }
