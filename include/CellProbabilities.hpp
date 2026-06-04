cf3be475d98fb8f6118f24d4417118d12ec3f6ec
sz3kz <139809645+sz3kz@users.noreply.github.com>
2026-06-04 12:41:21 +0200

    refactor: implement default probability construction

--- include/CellProbabilities.hpp
+++ include/CellProbabilities.hpp
@@ -2,6 +2,48 @@
 #include "World.hpp"
 #include <map>
 
+namespace EmptyCellDefaultProbabilities
+{
+inline constexpr double CreateNewHouse{ 0.001 };
+inline constexpr double CreateNewFirestation{ 0.00001 };
+inline constexpr double CreateNewShop{ 0.00001 };
+inline constexpr double CreateNewFactory{ 0.00001 };
+inline constexpr double CreateNewChurch{ 0.00001 };
+inline constexpr double SetCurrentBuildingOnFire{ 0.0 };
+inline constexpr double RestoreBuildingFromRuin{ 0 };
+inline constexpr double RestoreBuildingTimeToLive{ 0 };
+}
+
+namespace OccupiedCellDefaultProbabilities
+{
+inline constexpr double CreateNewHouse{ 0.0 };
+inline constexpr double CreateNewFirestation{ 0.0 };
+inline constexpr double CreateNewShop{ 0.0 };
+inline constexpr double CreateNewFactory{ 0.0 };
+inline constexpr double CreateNewChurch{ 0.0 };
+inline constexpr double SetCurrentBuildingOnFire{ 0.0001 };
+inline constexpr double RestoreBuildingFromRuin{ 0 };
+inline constexpr double RestoreBuildingTimeToLive{ 0 };
+}
+
+enum class CellState : uint8_t
+{
+    Empty,
+    Occupied
+};
+
+enum class ProbabilityType : std::uint8_t
+{
+    CREATE_NEW_HOUSE,
+    CREATE_NEW_FIRESTATION,
+    CREATE_NEW_SHOP,
+    CREATE_NEW_FACTORY,
+    CREATE_NEW_CHURCH,
+    SET_CURRENT_BUILDING_ON_FIRE,
+    RESTORE_TIME_TO_LIVE,
+    RESTORE_FROM_RUIN,
+};
+
 struct Probability
 {
     unsigned long last_updated_at_iteration;
@@ -11,5 +53,7 @@ struct Probability
 struct CellProbabilities
 {
     std::map<ProbabilityType, Probability> probabilities;
-    CellProbabilities();
+    explicit CellProbabilities(CellState state);
+    CellProbabilities()
+      : CellProbabilities(CellState::Empty) {};
 };
--- include/World.hpp
+++ include/World.hpp
@@ -35,29 +35,6 @@ enum class BuildingState : std::uint8_t
     RUIN,
 };
 
-enum class ProbabilityType : std::uint8_t
-{
-    CREATE_NEW_HOUSE,
-    CREATE_NEW_FIRESTATION,
-    CREATE_NEW_SHOP,
-    CREATE_NEW_FACTORY,
-    CREATE_NEW_CHURCH,
-    SET_CURRENT_BUILDING_ON_FIRE,
-    RESTORE_TIME_TO_LIVE,
-    RESTORE_FROM_RUIN,
-};
-
-inline std::map<ProbabilityType, double> probability_default_percentages = {
-    { ProbabilityType::CREATE_NEW_HOUSE, 0.001 },
-    { ProbabilityType::CREATE_NEW_FIRESTATION, 0.00001 },
-    { ProbabilityType::CREATE_NEW_SHOP, 0.00001 },
-    { ProbabilityType::CREATE_NEW_FACTORY, 0.00001 },
-    { ProbabilityType::CREATE_NEW_CHURCH, 0.00001 },
-    { ProbabilityType::SET_CURRENT_BUILDING_ON_FIRE, 0.0001 },
-    { ProbabilityType::RESTORE_FROM_RUIN, 0 },
-    { ProbabilityType::RESTORE_TIME_TO_LIVE, 0 }
-};
-
 constexpr double set_adjacent_building_on_fire = 0.8;
 
 constexpr unsigned int default_properties_house_radius = 2;
